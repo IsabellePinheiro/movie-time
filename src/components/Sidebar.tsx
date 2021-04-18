3da919c69f161a3d93d0d6d1a04f213e80fa1eb1
IsabellePinheiro <isabellepinheiro00@gmail.com>
2021-04-17 21:06:37 -0300

    implement sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+
+import "../styles/sidebar.scss";
+
 export default function SideBar() {
-  return <div></div>;
+  const [genres, setGenres] = useState(["Action", "Comedy", "Documentary"]);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Movie<p>Time</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre, key) => (
+          <button key={key}>{genre}</button>
+        ))}
+      </div>
+    </nav>
+  );
 }
